5a7baa5755c626658db7b2144b7bc1c8c9a0d8a9
Adrian <mahdigh@live.com>
2023-12-19 10:24:00 +0330

    refactor(project): proper structure given to server. index.ts and routes now in src dir.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import patientsRouters from "./routes/patientsRouters"
+const cors = require('cors')
+
 const app = express();
 app.use(express.json());
+app.use(cors())
+const PORT = 3001;
 
-const PORT = 3000;
-
-app.get("/ping", (_req, res) => {
+app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use('/api/patients', patientsRouters);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
