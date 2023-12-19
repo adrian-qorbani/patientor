eaa554803578e86788d161525ea278a04626df52
Adrian <mahdigh@live.com>
2023-12-19 12:04:21 +0330

    fix(data): patients types changed so hardcoded data is revised.

--- data/patients_entries.ts
+++ data/patients_entries.ts
@@ -2,32 +2,44 @@ import { PatientEntry } from "../src/types/types";
 
 const patientsData: PatientEntry[] = [
   {
-    "id": 1,
-    "name": "John Doe",
-    "gender": "Male",
-    "occupation": "Engineer",
-    "healthRating": 4
+      "id": "d2773336-f723-11e9-8f0b-362b9e155667",
+      "name": "John McClane",
+      "dateOfBirth": "1986-07-09",
+      "ssn": "090786-122X",
+      "gender": "Male",
+      "occupation": "New york city cop"
   },
   {
-    "id": 2,
-    "name": "Jane Smith",
-    "gender": "Female",
-    "occupation": "Doctor",
-    "healthRating": 5
+      "id": "d2773598-f723-11e9-8f0b-362b9e155667",
+      "name": "Martin Riggs",
+      "dateOfBirth": "1979-01-30",
+      "ssn": "300179-77A",
+      "gender": "Male",
+      "occupation": "Cop"
   },
   {
-    "id": 3,
-    "name": "Michael Johnson",
-    "gender": "Male",
-    "occupation": "Teacher",
-    "healthRating": 3
+      "id": "d27736ec-f723-11e9-8f0b-362b9e155667",
+      "name": "Hans Gruber",
+      "dateOfBirth": "1970-04-25",
+      "ssn": "250470-555L",
+      "gender": "Other",
+      "occupation": "Technician"
   },
   {
-    "id": 4,
-    "name": "Samantha Brown",
-    "gender": "Female",
-    "occupation": "Artist",
-    "healthRating": 4
+      "id": "d2773822-f723-11e9-8f0b-362b9e155667",
+      "name": "Dana Scully",
+      "dateOfBirth": "1974-01-05",
+      "ssn": "050174-432N",
+      "gender": "Female",
+      "occupation": "Forensic Pathologist"
+  },
+  {
+      "id": "d2773c6e-f723-11e9-8f0b-362b9e155667",
+      "name": "Matti Luukkainen",
+      "dateOfBirth": "1971-04-09",
+      "ssn": "090471-8890",
+      "gender": "Male",
+      "occupation": "Digital evangelist"
   }
 ]
 ;
