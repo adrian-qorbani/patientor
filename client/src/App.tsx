ddd4c957c108dcb65a729c1553bbdd024ec699c1
Adrian <mahdigh@live.com>
2023-12-19 12:49:59 +0330

    refactor(client): removed useless dummy request sender from frontend.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,7 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { Button, Divider, Container, Typography } from '@mui/material';
 
-import { apiBaseUrl } from "./constants";
 import { Patient } from "./types";
 
 import patientService from "./services/patients";
@@ -13,8 +11,6 @@ const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
 
   useEffect(() => {
-    void axios.get<void>(`${apiBaseUrl}/ping`);
-
     const fetchPatientList = async () => {
       const patients = await patientService.getAll();
       setPatients(patients);
