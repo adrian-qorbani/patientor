0f0c281f024dee527698959d87b60fd0f284e1bd
Adrian <mahdigh@live.com>
2023-12-26 12:24:25 +0330

    test(login): added two test for new login route.

--- src/__tests__/userRouter.test.ts
+++ src/__tests__/userRouter.test.ts
@@ -3,15 +3,16 @@ import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import app from "../app"; // Assuming your Express app is defined in a file called 'app.ts'
 import { User } from "../models/user";
+import bcrypt from "bcrypt";
 
 let mongoServer: MongoMemoryServer;
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
   const mongoUri = mongoServer.getUri();
-  
+
   await mongoose.connect(mongoUri);
-})
+});
 
 afterAll(async () => {
   await mongoose.disconnect();
@@ -69,5 +70,50 @@ describe("User Registration", () => {
 
     expect(response.body.status).toBe(400);
     expect(response.body.message).toBe("Email already taken. Try another.");
+  });
+});
+
+describe("Login Route", () => {
+  it("should authenticate a user and return a valid token", async () => {
+    // Create a test user
+    const newUser = {
+      username: "testuser",
+      password: await bcrypt.hash("testpassword", 10), // Assuming you have bcrypt in scope
+      email: "myemail@gmail.com",
+    };
+    await User.create(newUser);
+
+    // Make a request to the login route
+    const response = await request(app)
+      .post("/auth/login")
+      .send({
+        username: "testuser",
+        password: "testpassword",
+      })
+      .expect(200);
+
+    // Ensure the response structure is as expected
+    expect(response.body).toHaveProperty("token");
+    expect(response.body).toHaveProperty("username", "testuser");
+    expect(response.body).toHaveProperty("name"); // Assuming your user model has a 'name' field
+
+    // You can add more assertions as needed based on your actual response structure
+  });
+
+  it("should return a 401 for an invalid username or password", async () => {
+    // Make a request to the login route with invalid credentials
+    const response = await request(app)
+      .post("/auth/login")
+      .send({
+        username: "nonexistentuser",
+        password: "invalidpassword",
+      })
+      .expect(401);
+
+    // Ensure the response structure is as expected for an authentication failure
+    expect(response.body).toHaveProperty(
+      "error",
+      "Invalid username or password."
+    );
   });
 });
