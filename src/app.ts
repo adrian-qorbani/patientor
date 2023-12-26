c6e64929ee9ee661e88658e5af52925019b50fea
Adrian <mahdigh@live.com>
2023-12-26 21:47:42 +0330

    refactor(app): removed redundant lines and refactored app.ts.

--- src/app.ts
+++ src/app.ts
@@ -8,26 +8,15 @@ import diagnosesRouters from './routes/diagnosesRouter';
 import userRouter from './routes/userRouter'
 import loginRouter from './routes/loginRouter'
 
-
 const app: Application = express();
 
-// const app = express();
 app.use(cors());
 dotenv.config();
 app.use(express.json());
-
-
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/api/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.status(200).send('pong');
-});
-
 app.use('/api/patients', patientsRouters);
 app.use('/api/diagnoses', diagnosesRouters);
 app.use('/', userRouter)
 app.use('/', loginRouter)
-
 
 export default app;
